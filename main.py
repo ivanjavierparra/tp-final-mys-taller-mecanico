1cf0f49fb1b9cffb03d9698d37c756366783ac7e
ian <ianmazzaglia@gmail.com>
2018-06-06 18:28:32 -0300

    primera aproximacion del main

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+from modelo import Elevador, Evento, Galpon, Mecanico, Reloj, Reparacion, Taller, Vhiculo
+
+colaEntrada = []
+colaEventos = []
+
+def main(argumentos):
+    reloj = Reloj()
+    galpon = Galpon()
+    mecanico = Mecanico()
+    reparacion = Reparacion()
+    elevador = Elevador()
+
+    generarColaEntrada(colaEntrada)
+    
+    while(10 >= reloj.get_valor() and reloj.get_valor =< 20)
+        if (len(galpon.espacio) > 0 )
+            if (mecanico.disponible)
+                reparacion(galpon.espacio.first(), mecanico, elevador)
+                colaEventos.append(evento)
+        if (!galpon.esta_lleno)
+            galpon.espacio.append(colaEntrada.first())
+        //ejecutar Proximo Evento
